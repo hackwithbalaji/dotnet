07fb795de4ca69218b91901f7d23165c95ec7716
Balaji <hackwithbalaji@gmail.com>
2021-03-10 13:47:21 +0530

    initial commit

---
+++ product_crud/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace product_crud.Controllers
+{
+    [ApiController]
+    [Route("demo")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "demo";
+        }
+    }
+}
